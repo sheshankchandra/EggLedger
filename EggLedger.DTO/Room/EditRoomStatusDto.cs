c4df242e56c54c5783f12db1a7b89ff181dbc748
tharunthej <tharunthej989@gmail.com>
2025-07-27 10:14:28 +0530

    BE - Added end point for editing room status

---
+++ EggLedger.DTO/Room/EditRoomStatusDto.cs
@@ -0,0 +1,10 @@
+﻿using EggLedger.Models.Enums;
+
+namespace EggLedger.DTO.Room
+{
+    public class EditRoomStatusDto
+    {
+        public required Guid RoomId { get; set; }
+        public required RoomStatus NewStatus { get; set; }
+    }
+}
